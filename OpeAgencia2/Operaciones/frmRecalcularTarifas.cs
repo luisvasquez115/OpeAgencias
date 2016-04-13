ca845febdc07b9bf182de27e6444bb953d29e89b
manfredoferreras@gmail.com <manfredoferreras@gmail.com>
2016-04-13 11:55:17 -0400

    Se corrige error en recalcular itebis en recalcular tarifa

--- OpeAgencia2/Operaciones/frmRecalcularTarifas.cs
+++ OpeAgencia2/Operaciones/frmRecalcularTarifas.cs
@@ -52,7 +52,7 @@ namespace OpeAgencia2.Operaciones
                 AgenciaEF_BO.DAL.ADO.BultosDal Bultos = new BO.DAL.ADO.BultosDal();
 
                 Bultos.RecalcularUnidades(oBulto.BLT_NUMERO);
-                Bultos.RecalcularItebis(oBulto.BLT_NUMERO);
+                //Bultos.RecalcularItebis(oBulto.BLT_NUMERO);
                 BO.BO.Facturar oFact = new BO.BO.Facturar();
                 oFact.ActualizarItbis(oBulto.BLT_NUMERO);
                 
